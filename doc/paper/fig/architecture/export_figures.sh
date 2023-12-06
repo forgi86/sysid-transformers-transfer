c219bd58cda71400b5204eafa6e3a0d95e2751ca
Marco Forgione <marco.forgione@supsi.ch>
2023-12-06 15:47:22 +0100

    deleted old paper

--- doc/paper/fig/architecture/export_figures.sh
+++
@@ -1,3 +0,0 @@
-alias inkscape="/Applications/Inkscape.app/Contents/MacOS/inkscape"
-inkscape *.svg  --export-area-drawing --export-type="pdf"
-inkscape *.svg  --export-area-drawing --export-dpi=400 --export-type="png"
